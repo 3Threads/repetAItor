9a3b56591c56d562fe21796af78442eb2211eeec
Niko Khetsuriani <nkhet21@freeuni.edu.ge>
2024-02-07 00:54:59 +0400

    Update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,60 @@
-import React from 'react';
-import {Button, Container, Nav, Navbar} from 'react-bootstrap';
+import React, { useEffect } from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import '../App.css';
+import { Tabs } from "@mantine/core";
+import { useParams } from "react-router-dom";
 
 export const Header: React.FC = () => {
+    const { subject } = useParams();
+
+    useEffect(() => {
+        document.documentElement.setAttribute('data-mantine-color-scheme', 'dark');
+    }, []);
+
     return (
-        <Navbar expand="lg" variant="dark" className={"mb-4"}>
+        <Navbar expand="lg" variant="dark" className={"mb-4 custom-navbar"}>
             <Container>
                 <Navbar.Brand href="/">
                     <h1>
-                        <span style={{color: 'orange'}}>A</span>biturient<span style={{color: 'orange'}}>I</span>
+                        <span style={{ color: 'orange' }}>A</span>biturient<span style={{ color: 'orange' }}>I</span>
                     </h1>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/tests/english" className={'p-0 m-1'}><Button
-                            variant={'warning'}>ინგლისური</Button></Nav.Link>
-                        <Nav.Link href="/tests/georgian" className={'p-0 m-1'}><Button
-                            variant={'warning'}>ქართული</Button></Nav.Link>
-                        <Nav.Link href="/tests/math" className={'p-0 m-1'}><Button
-                            variant={'warning'}>მათემატიკა</Button></Nav.Link>
-                        <Nav.Link href="/tests/history" className={'p-0 m-1'}><Button
-                            variant={'warning'}>ისტორია</Button></Nav.Link>
-                    </Nav>
-                    <Navbar.Brand href="#contact-us">
+
+                    <div style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+                        <Tabs value={subject} color="yellow" radius="xs">
+                            <Tabs.List >
+                                <a href={"/tests/georgian"} style={{ textDecoration: 'none', color: subject === "georgian" ? '#f08c00' : 'white' }}>
+                                    <Tabs.Tab value="georgian">
+                                        ქართული
+                                    </Tabs.Tab>
+                                </a>
+                                <a href={"/tests/english"} style={{ textDecoration: 'none', color: subject === "english" ? '#f08c00' : 'white' }}>
+                                    <Tabs.Tab value="english">
+                                        ინგლისური
+                                    </Tabs.Tab>
+                                </a>
+                                <a href={"/tests/math"} style={{ textDecoration: 'none', color: subject === "math" ? '#f08c00' : 'white' }}>
+                                    <Tabs.Tab value="math">
+                                        მათემატიკა
+                                    </Tabs.Tab>
+                                </a>
+                                <a href={"/tests/history"} style={{ textDecoration: 'none', color: subject === "history" ? '#f08c00' : 'white' }}>
+                                    <Tabs.Tab value="history">
+                                        ისტორია
+                                    </Tabs.Tab>
+                                </a>
+                            </Tabs.List>
+                        </Tabs>
+                    </div>
+
+                    <Navbar.Brand>
+                        <Button variant="outline-warning" className={'m-1'}>Sign in</Button>
                         <Button variant="outline-warning" className={'m-1'}>Contact Us</Button>
                     </Navbar.Brand>
                 </Navbar.Collapse>
-
             </Container>
         </Navbar>
     );
 }
-
-
--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import '@mantine/core/styles.css';
+import {MantineProvider, createTheme} from "@mantine/core";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
+
+const theme = createTheme({
+    /** Put your mantine theme override here */
+});
+
+
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
+    <MantineProvider theme={theme}>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </MantineProvider>
 );
 
